ffedddf8e1ebdcf47c6d8a5d17422d080846540e
lyMeiSEU <42669308+lyMeiSEU@users.noreply.github.com>
2018-11-17 12:06:39 +0800

    Create P287-3.cpp

---
+++ P287-3.cpp
@@ -0,0 +1,102 @@
+#include<iostream>
+#include<time.h>
+#include<queue>
+#include<stack>
+using namespace std;
+
+#define ElementType int
+typedef struct TreeNode *BinTree;
+typedef BinTree Position;
+struct  TreeNode
+{
+	ElementType Data;
+	BinTree father;
+	BinTree Left;
+	BinTree Right;
+};
+
+void NodeInserter(BinTree in) {
+	if (in) {
+		if (in->father->Data < in->Data)
+		{
+			BinTree inT = (BinTree)malloc(sizeof(struct TreeNode));
+			BinTree faT = (BinTree)malloc(sizeof(struct TreeNode));
+			inT->Data = in->Data;
+			inT->father = in->father;
+			inT->Left = in->Left;
+			inT->Right = in->Right;
+			faT->Data = in->father->Data;
+			faT->father = in->father->father;
+			faT->Left = in->father->Left;
+			faT->Right = in->father->Right;
+			in->father = inT;
+			in = faT;
+			NodeInserter(in->father);
+		}
+	}
+}
+static int n=0;
+BinTree InOrderTraversal(BinTree BT) {
+	BinTree T = BT;
+	stack<BinTree>s;
+	while (T != NULL || s.size() != 0)
+	{
+		while (T != NULL) {
+			s.push(T);
+			cout << s.top()->Data << " ";
+			T = T->Left;
+		}
+		if (s.size() != 0)
+		{
+			T = s.top();
+			s.pop();
+			T = T->Right;
+		}
+	}
+	return BT;
+}
+
+BinTree TreeBulider(int now, int N,BinTree father,BinTree &last) {
+	if (now <= N) {
+		BinTree tmp = (BinTree)malloc(sizeof(struct TreeNode));
+		tmp->Data = N - now + 1;
+		tmp->father = father;
+		tmp->Left = TreeBulider(2 * now, N,tmp,last);
+		tmp->Right = TreeBulider(2 * now + 1, N, tmp,last);
+		if (now == N)
+		{
+			last = tmp;
+		}
+		return tmp;
+	}
+	else
+		return NULL;
+}
+
+int main() {
+	BinTree BT = (BinTree)malloc(sizeof(struct TreeNode));
+	BinTree last = (BinTree)malloc(sizeof(struct TreeNode));
+	BT->father = NULL;
+	int N;
+	cout << "Input the Node number in the Tree" << endl;
+	cin >> N;
+	cout << "Buliding Tree..." << endl;/*
+										 1
+									   2   3
+									 4  5 6  7
+									...........
+									   */
+	BT = TreeBulider(1, N,BT,last);
+	InOrderTraversal(BT);
+	int S;
+	cout << "Input the Node you want to Insert" << endl;
+	cin >> S;
+	cout << "Building Node..." << endl;
+	BinTree in = (BinTree)malloc(sizeof(struct TreeNode));
+	in->father = last;
+	in->Data = S;
+	in->Left = NULL;
+	in->Right = NULL;
+	NodeInserter(in);
+	InOrderTraversal(BT);
+}
